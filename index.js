45241335aab808ec719aa5bd5014f0d82d13efe0
Richardson25 <egaliterichardson@gmail.com>
2023-10-31 17:12:48 -0700

    Homework_Queue_with_Javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+//QUEUE: Fifo, first-in, first-out
+
+let queue = []
+
+queue.push(1)
+queue.push(2)
+queue.push(3)
+queue.push(4)
+queue.push(5)
+
+let retire = queue.shift()
+
+console.log(queue)
+
+console.log(retire)
+
+
+
+
+
+
+
